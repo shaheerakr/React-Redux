5bcb2ad9c1804a7fad2785671574258140aa631c
Shaheer Akram <shaheerakram@icloud.com>
2019-10-06 22:43:27 +0500

    add new tasks

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ShowTasks from './showTasks'
+import AddTask from './addTask'
 
 class App extends Component {
   state = {
@@ -7,6 +8,14 @@ class App extends Component {
       {id : 0, task : "brush"},
       {id : 1, task : "Get bread"}
     ]
+  }
+  addTask = (task) =>{
+    //console.log(task)
+    task.id = Math.random()
+    let todos = [...this.state.todos,task]
+    this.setState({
+      todos
+    })
   }
   deleteTodo = (id) =>{
     //window.alert("do you realy want to delete this item?")
@@ -20,10 +29,11 @@ class App extends Component {
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <ShowTasks todo = {this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTask addTask = {this.addTask}/>
         <p className="center blue-text">Click on the tasks you want to remove</p>
       </div>
     );
-}
+  }
 }
 
 export default App;
---
+++ todo/src/addTask.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class AddTask extends Component{
+    state ={
+        task : ""
+    }
+    handleChange = (e) => {
+        this.setState({
+            task : e.target.value
+        })
+    }
+    handleSubmit = (e) =>{
+        e.preventDefault()
+        this.props.addTask(this.state)
+        this.setState({
+            task : ""
+        })
+    }
+    render(){
+        return (
+            <div>
+                <form onSubmit = {this.handleSubmit}>
+                    Add new Task: 
+                    <input type="text" onChange ={this.handleChange} value={this.state.task}/>
+                    <button className="btn waves-effect blue" type="submit" name="action">
+                        Add Task
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTask
