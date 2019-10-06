bb19d0930f29e1acc1dedb502961c387395d5b34
Shaheer Akram <shaheerakram@icloud.com>
2019-10-06 20:30:58 +0500

    basic app structure

---
+++ todo/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    todo : [
+      {id : 0, task : "brush"},
+      {id : 1, task : "Get bread"}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+}
+}
+
+export default App;
